ac50f33ad75638fbcf693314173ccebff278dd24
Han Wen Fu <106775693+Hanoween@users.noreply.github.com>
2022-08-17 22:34:33 -0400

    Update rpsScript.js

--- rpsScript.js
+++ rpsScript.js
@@ -1,4 +1,5 @@
 const choices = ["Rock", "Paper", "Scissors"];
+var scoreBoard = { "Win": 0, "Lose": 0 }
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -18,24 +19,48 @@ function playRound(playSelection, computerSelection) {
     }
 }
 
+function setButtons() {
+    const btn = document.querySelectorAll("button");
+    const div = document.querySelector(".game");
+    var results = document.querySelector(".results");
+
+    btn.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            results.remove();
+            results = document.createElement("div")
+            results.classList.add("results");
+            results.setAttribute("style", "font-size: 50px; padding-top: 75px; color: blue; font-weight: 700;");
+            output = playRound(btn.innerHTML.split("\n")[0], getComputerChoice());
+            const content = document.createElement("p");
+            content.innerHTML = output
+            results.appendChild(content);
+            div.appendChild(results);
+            game(output);
+        });
+    });
+}
+
 function game(output) {
     if (output.includes("Win")) {
-        if (++scoreBoard["Win"] == 5) alert("You Win!");
+        if (++scoreBoard["Win"] == 5) {
+            scoreBoard = { "Win": 0, "Lose": 0 }
+            alert("You Win!");
+            if (confirm("Restart?")) {
+                results.remove();
+                setButtons();
+            }
+        }
     }
     else if (output.includes("Lose")) {
-        if (++scoreBoard["Lose"] == 5) alert("You Lose!");
+        if (++scoreBoard["Lose"] == 5) {
+            scoreBoard = { "Win": 0, "Lose": 0 }
+            alert("You Lose!");
+            if (confirm("Restart?")) {
+                results.remove();
+                setButtons();
+            }
+        }
     }
 }
 
-const btn = document.querySelectorAll('#choice');
-var scoreBoard = { "Tie": 0, "Win": 0, "Lose": 0 }
-
-btn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        output = playRound(btn.innerHTML, getComputerChoice());
-        game(output)
-        const result = document.createElement('div');
-        result.innerHTML = output;
-        document.body.appendChild(result);
-    });
-});
+setButtons();
